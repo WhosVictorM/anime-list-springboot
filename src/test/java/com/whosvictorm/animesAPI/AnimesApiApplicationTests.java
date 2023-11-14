ce3d86b2393fdedfaf2ce0e099f23711f66ce7a7
Victor <victormagalhaespb@gmail.com>
2023-11-14 09:25:55 -0300

    Configuração e início do programa

---
+++ src/test/java/com/whosvictorm/animesAPI/AnimesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whosvictorm.animesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
