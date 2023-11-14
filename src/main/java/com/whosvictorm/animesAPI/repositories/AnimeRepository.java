ddc4ddc40934d4c532da1db2472955c0ef484281
Victor <victormagalhaespb@gmail.com>
2023-11-14 09:42:54 -0300

    Criação do Repository

---
+++ src/main/java/com/whosvictorm/animesAPI/repositories/AnimeRepository.java
@@ -0,0 +1,7 @@
+package com.whosvictorm.animesAPI.repositories;
+
+import com.whosvictorm.animesAPI.entities.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+}
